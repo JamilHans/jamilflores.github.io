9135bf531ae901015c3122914ff62da04187bb75
Jamil Flores <extjflores@belcorp.biz>
2024-06-03 00:54:36 -0500

    Add changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.text', {
-    strings: ["Android Developer", "Web Developer"],
+    strings: ["QA - Tester","Android Developer", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
